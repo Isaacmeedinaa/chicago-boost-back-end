ae8e099899931e2d95764b7509d831bce9e72f12
Isaac Medina <isaac.meedinaa@gmail.com>
2021-03-10 15:10:45 -0600

    added front end and ready for deployment

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const cors = require("cors");
 const database = require("./config/database");
 const routes = require("./config/routes");
 const cloudinary = require("./config/cloudinary");
-
+const path = require("path");
 const app = express();
 
 app.use(cors());
 database();
 cloudinary();
 routes(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 7000;
 
