300ed04a155dea0733796166ee386163dd8e9ac8
Isaac Medina <isaac.meedinaa@gmail.com>
2021-03-10 18:59:00 -0600

    fixed?

--- server.js
+++ server.js
@@ -13,14 +13,26 @@ require("./config/routes")(app);
 require("./config/database")();
 require("./config/cloudinary")();
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, "client/build")));
 
-  // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   // Handle React routing, return all requests to React app
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
+
+if (process.env.NODE_ENV === "production") {
+  // serve the static react app
+  app.use(express.static("client/build"));
+
+  // don't serve api routes to react app
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
+
+  console.log("Serving React App...");
 }
 
 app.listen(port, () => console.log(`Listenining on port ${port} 🌎!`));
