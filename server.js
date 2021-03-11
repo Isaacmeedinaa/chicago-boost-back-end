49a277dd3e31ca09d1ab40c5e4e94daf0cdbfc69
Isaac Medina <isaac.meedinaa@gmail.com>
2021-03-10 18:25:20 -0600

    ready to deploy

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const database = require("./config/database");
-const routes = require("./config/routes");
-const cloudinary = require("./config/cloudinary");
 const path = require("path");
+
 const app = express();
 
+const port = process.env.PORT || 7000;
+
 app.use(cors());
-routes(app);
-database();
-cloudinary();
+
+require("./config/routes")(app);
+require("./config/database")();
+require("./config/cloudinary")();
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
 
   // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  // });
 }
-
-const port = process.env.PORT || 7000;
 
 app.listen(port, () => console.log(`Listenining on port ${port} 🌎!`));
